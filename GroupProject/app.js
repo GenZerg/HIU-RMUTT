759e85f393b8c42960160854147a82c5343ae81b
Kaho.M <s2021075@s.do-johodai.ac.jp>
2022-12-27 14:00:20 +0700

    change style.css

--- GroupProject/app.js
+++ GroupProject/app.js
@@ -1,3 +1,10 @@
 function onClickMenu(){
     document.getElementById("menu").classList.toggle("change");
 }
+
+window.addEventListener("load", () => {
+    document.querySelector(".loader").classList.add("loader--hidden");
+    document.querySelector(".loader").addEventListener("transitioned", () => {
+        document.body.removeChild(document.querySelector(".loader"));
+    });
+})
